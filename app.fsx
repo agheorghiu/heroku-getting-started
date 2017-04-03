3769b9a16db1557b03108e904618ae54ed92e4bc
Andru Gheorghiu <gheorghiuandru@gmail.com>
2017-04-03 01:08:28 +0100

    Updated app

--- app.fsx
+++ app.fsx
@@ -27,13 +27,49 @@ module Script =
         M [q]
 
 
-    let test(pad:int, n:int)    =
+    let test(pad:int, n:int, angle1, angle2, angle3)    =
         let mutable out1 = 0
         let mutable out2 = 0
         let mutable out3 = 0
         let mutable out4 = 0
         let mutable outout = 0
         let k = Ket(4)
+        let mutable rangle1 = Math.PI / 2.
+        let mutable rangle2 = Math.PI / 4.
+        let mutable rangle3 = Math.PI / 2.
+
+        if (angle1 % 4) = 0 then
+            rangle1 <- 0.
+        else
+            if (angle1 % 4) = 1 then
+                rangle1 <- Math.PI / 2.
+            else
+                if (angle1 % 4) = 2 then
+                    rangle1 <- Math.PI
+                else
+                    rangle1 <- 3. * Math.PI / 2.
+
+        if (angle2 % 4) = 0 then
+            rangle2 <- Math.PI / 4.
+        else
+            if (angle2 % 4) = 1 then
+                rangle2 <- -Math.PI / 4.
+            else
+                if (angle2 % 4) = 2 then
+                    rangle2 <- Math.PI / 2.
+                else
+                    rangle2 <- -Math.PI / 2.
+                    
+        if (angle3 % 4) = 0 then
+            rangle3 <- 0.
+        else
+            if (angle3 % 4) = 1 then
+                rangle3 <- Math.PI / 2.
+            else
+                if (angle3 % 4) = 2 then
+                    rangle3 <- Math.PI
+                else
+                    rangle3 <- 3. * Math.PI / 2.
 
         //let pad = 0
         for i in 1..n do
@@ -49,9 +85,9 @@ module Script =
             CZ [qs.[1]; qs.[2]]
 
             // server measurements
-            rotatedMeasurement qs.[0] (Math.PI / 2.)
-            rotatedMeasurement qs.[1] (Math.PI / 4.)
-            rotatedMeasurement qs.[2] (Math.PI / 2.)
+            rotatedMeasurement qs.[0] rangle1
+            rotatedMeasurement qs.[1] rangle2
+            rotatedMeasurement qs.[2] rangle3
 
             //client measurement
             if ((qs.[0].Bit.v + pad) % 2) = 0 then                
@@ -74,11 +110,6 @@ module Script =
         [outout; out1; out2; out3; out4]
 
 
-
-let n = 1000
-let results0 = Script.test(0, n)
-let results1 = Script.test(1, n)
-
 let htmlf = (sprintf """
   <html>
   <head>
@@ -92,8 +123,6 @@ let htmlf = (sprintf """
             Plotly.newPlot('61f0250d-dbf5-43eb-b33b-b2c348c3acdc', data, layout);
         </script></body></html>
 """)
-let html = (htmlf results0.[0] results0.[1] results0.[2] results0.[3] results0.[4] results1.[0] results1.[1] results1.[2] results1.[3] results1.[4] n)
-
 
 let config = 
     let port = System.Environment.GetEnvironmentVariable("PORT")
@@ -105,5 +134,10 @@ let config =
         bindings=[ (if port = null then HttpBinding.mk HTTP ip127 (uint16 8080)
                     else HttpBinding.mk HTTP ipZero (uint16 port)) ] }
 
-let app = OK html
+let f n angle1 angle2 angle3 = async {
+    return Script.test(0, n, angle1, angle2, angle3) @ Script.test(1, n, angle1, angle2, angle3)
+}
+
+let app =
+  pathScan "/%d-%d-%d-%d" (fun (n : int, angle1 : int, angle2 : int, angle3 : int) -> fun x -> async{ let! r = (f n angle1 angle2 angle3) in return! OK (htmlf r.[0] r.[1] r.[2] r.[3] r.[4] r.[5] r.[6] r.[7] r.[8] r.[9] n) x })
 startWebServer config app
